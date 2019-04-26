3ab030db2274cc07e73d8143c37c49db2fac4f4b
Samuel Maturo <samuel.maturo@gmail.com>
2019-04-26 00:30:36 +0000

    MVP complete

--- script.js
+++ script.js
@@ -17,13 +17,17 @@ $.ajax({
 });
 $("#match1home").click(function(){
     $("#result").html(match1home);
+    $("#image").html("<img src = http://www.stickpng.com/assets/images/580b57fcd9996e24bc43c4ee.png></img>");
 });
 $("#match1away").click(function(){
     $("#result").html(match1away);
+    $("#image").html("<img src = http://www.freelogovectors.net/wp-content/uploads/2018/08/ajax-logo.png></img>");
 });
 $("#match2home").click(function(){
     $("#result").html(match2home);
+    $("#image").html("<img src = https://upload.wikimedia.org/wikipedia/en/thumb/4/47/FC_Barcelona_%28crest%29.svg/1200px-FC_Barcelona_%28crest%29.svg.png></img>");
 });
 $("#match2away").click(function(){
     $("#result").html(match2away);
+    $("#image").html("<img src = https://upload.wikimedia.org/wikipedia/hif/b/bd/Liverpool_FC.png></img>");
 });
